fbcd564fd0bf14cbb0425d1821759697e06f7465
Andrew <andrewmakoviichuk@gmail.com>
2020-09-06 16:33:36 +0300

    Lab 3

---
+++ OOP Lab 3/03. Oldest Family Member/Family.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace OldestFamilyMember
+{
+    public class Family
+    {
+        private List<Person> familyMembers;
+
+        public Family()
+        {
+            familyMembers = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            familyMembers.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestMember = null;
+
+            foreach (var member in familyMembers)
+                if (oldestMember == null || member.Age > oldestMember.Age)
+                    oldestMember = member;
+
+            return oldestMember;
+        }
+    }
+}
---
+++ OOP Lab 3/04. Opinion Poll/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace OpinionPoll
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Person() : this(1)
+        {
+        }
+
+        public Person(int age) : this("No name", age)
+        {
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
